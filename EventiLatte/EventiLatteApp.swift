94fab3158873cb9f192fccbd4fc80a841f99d65d
themaanas <themaanas@gmail.com>
2023-08-27 23:32:38 -0400

    Initial Commit

---
+++ EventiLatte/EventiLatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventiLatteApp.swift
+//  EventiLatte
+//
+//  Created by Maanas Manoj on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct EventiLatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
