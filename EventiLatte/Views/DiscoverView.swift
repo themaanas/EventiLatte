8330786adaf2cea29aa4259e179c9423eef669f6
themaanas <themaanas@gmail.com>
2023-08-31 18:29:55 -0400

    `discover

--- EventiLatte/Views/DiscoverView.swift
+++ EventiLatte/Views/DiscoverView.swift
@@ -8,94 +8,138 @@
 import SwiftUI
 import FeedKit
 
+extension View {
+    /// Hide or show the view based on a boolean value.
+    ///
+    /// Example for visibility:
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true)
+    ///
+    /// Example for complete removal:
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true, remove: true)
+    ///
+    /// - Parameters:
+    ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
+    ///   - remove: Boolean value indicating whether or not to remove the view.
+    @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
+            }
+        } else {
+            self
+        }
+    }
+}
+
 struct DiscoverView: View {
-    @State private var events: [[String]] = []
+    @State var events: [[String]] = []
     @State private var searchText = ""
-    @State private var searchIsActive = false
+    
+    @Environment(\.isSearching) var isSearching
     @State var articles: [String] = []
     var body: some View {
-        ExecuteCode {
-            
-        }
-        VStack {
-            
-            ZStack {
-                NavigationView {
-                    List {
-                        ForEach(articles, id: \.self) { article in
-                            Text(article)
-                        }
-                        
-                        .listRowSeparator(.hidden)
-                        
-                    }
-                    .listStyle(.plain)
+        
+        ZStack {
+            Color("colorBackground")
+                    .ignoresSafeArea()
+            VStack {
+                
+                
                     
-                }.searchable(text: $searchText)
+                
+                DiscoverContentView(events: $events, articles: $articles)
+                    .searchable(text: $searchText)
                     .onChange(of: searchText) { searchText in
+                    if !searchText.isEmpty {
+                        articles = events.compactMap { $0.first }.filter { $0.lowercased().contains(searchText.lowercased()) }
                         
-                        if !searchText.isEmpty {
-                            articles = events.compactMap { $0.first }.filter { $0.lowercased().contains(searchText.lowercased()) }
-                            
-                        } else {
-                            articles = []
-                        }
+                    } else {
+                        articles = []
                     }
-                    .padding(10)
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                
-                
-                ScrollView(.horizontal) {
-                    LazyHStack {
-                        if $events.count > 0 {
-                            ForEach(0...$events.count - 1, id: \.self) { index in
-                                Text(String($events[index].first!.wrappedValue))
-                                Text("hi")
-                                    .onAppear {
-                                        print(index)
-                                    }
-                                    .padding()
-                                    .background(.white)
-                                    .cornerRadius(8)
-                            }
-                        }
-                    }
-                }
-                .background(Color(UIColor.systemGroupedBackground))
-                .onAppear() {
-                    print(events)
-                    //                    events.removeLast()
-                    if events.count < 1 {
-                        let feedURL = URL(string: "https://rutgers.campuslabs.com/engage/events.rss")!
-                        let parser = FeedParser(URL: feedURL)
-                        let result = parser.parse()
-                        switch result {
-                        case .success(let feed):
-                            
-                            // Grab the parsed feed directly as an optional rss, atom or json feed object
-                            print(feed.rssFeed?.title)
-                            if let items = feed.rssFeed?.items {
-                                for item in items.prefix(10) {
-                                    events.append([(item.title)!, (item.description)!])
-                                    print(item)
-                                }
-                            }
-                            
-                            
-                            
-                            
-                        case .failure(let error):
-                            print(error)
-                        }
-                    }
-                    
-                }
+                    }.searchable(text: $searchText)
                 
                 
                 
             }
         }
         
+    }
+}
+struct DiscoverContentView: View {
+    @Environment(\.isSearching) private var isSearching
+    @Binding var events: [[String]]
+    @Binding var articles: [String]
+    @State private var searchIsActive = false
+    var body: some View {
+        NavigationView {
+            EmptyView()
+            if $searchIsActive.wrappedValue {
+                List {
+                    ForEach($articles.wrappedValue, id: \.self) { article in
+                        Text(article)
+                    }
+                    
+                    .listRowSeparator(.hidden)
+                    
+                }
+                .listStyle(.plain)
+            }
+        }
+        .padding(10)
+        ScrollView(.horizontal) {
+            LazyHStack {
+                if $events.count > 0 {
+                    ForEach(0...$events.count - 1, id: \.self) { index in
+                        Text(String($events[index].first!.wrappedValue))
+                        Text("hi")
+                            .onAppear {
+                                print(index)
+                            }
+                            .padding()
+                            .background(.white)
+                            .cornerRadius(8)
+                    }
+                }
+            }
+        }
+        .background(Color(UIColor.systemGroupedBackground))
+        .onAppear() {
+            print(events)
+            //                    events.removeLast()
+            if $events.count < 1 {
+                let feedURL = URL(string: "https://rutgers.campuslabs.com/engage/events.rss")!
+                let parser = FeedParser(URL: feedURL)
+                let result = parser.parse()
+                switch result {
+                case .success(let feed):
+                    
+                    // Grab the parsed feed directly as an optional rss, atom or json feed object
+                    print(feed.rssFeed?.title)
+                    print(feed.rssFeed?.items?.count)
+                    if let items = feed.rssFeed?.items {
+                        for item in items.prefix(10) {
+                            events.append([(item.title)!, (item.description)!])
+                            print(item)
+                        }
+                    }
+                    
+                    
+                    
+                    
+                case .failure(let error):
+                    print(error)
+                }
+            }
+            
+        }.isHidden($searchIsActive.wrappedValue)
+        .onChange(of: isSearching) { newValue in
+            searchIsActive = newValue
+                print(searchIsActive)
+            }
     }
 }
 
