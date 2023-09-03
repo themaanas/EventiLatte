e51316a54685722334c8436698f796d38d3f2927
Kevin Tran <thekevintran01@gmaill.com>
2023-09-03 02:10:21 -0400

    added commands to notiviews that sends notis to user * change code to show event as notificaiton*

--- EventiLatte/Views/SignInView.swift
+++ EventiLatte/Views/SignInView.swift
@@ -14,54 +14,61 @@ struct SignInView: View {
     
     @State private var loginSuccess = false
     var body: some View {
-        VStack {
+        ZStack{
             Color("colorBackground")
                 .ignoresSafeArea()
-            TextField("", text: $email)
-                .padding(.horizontal, 10)
-                .padding(.vertical, 8)
-                .foregroundColor(.black)
-                .textFieldStyle(.plain)
-                .placeholder(when: email.isEmpty) {
-                    Text("Email")
-                        .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255))
-                        .bold()
-                }
-                .background(Color(red: 230/255, green: 230/255, blue: 230/255))
-                .cornerRadius(7)
-                .frame(width: 200.0, height: 100.0)
-                .padding(.bottom, -50)
-                
-                
-            SecureField("", text: $password)
-                .padding(.horizontal, 10)
-                .padding(.vertical, 8)
-                .foregroundColor(.black)
-                .textFieldStyle(.plain)
-                .placeholder(when: password.isEmpty) {
-                    Text("Password")
-                        .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255))
-                        .bold()
-                }
-                .background(Color(red: 230/255, green: 230/255, blue: 230/255))
-                .cornerRadius(7)
-                .frame(width: 200.0, height: 100.0)
-            Button("Log In") {
-                print("hi")
-                register()
-            }
-            .alert("Message from SwiftUI", isPresented: $loginSuccess) {
-                        Button("Success") { }
+            VStack {
+                Text("Login")
+                    
+                TextField("", text: $email)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.black)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255))
+                            .bold()
                     }
-            .padding(10)
-            .frame(width:150)
-            .background(Color(red: 235/255, green: 136/255, blue: 66/255))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-            .padding(20)
-            
+                    .background(Color(red: 230/255, green: 230/255, blue: 230/255))
+                    .cornerRadius(7)
+                    .frame(width: 300.0, height: 100.0)
+                    .padding(.bottom, -50)
+                    
+                    
+                SecureField("", text: $password)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.black)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(Color(red: 200/255, green: 200/255, blue: 200/255))
+                            .bold()
+                    }
+                    .background(Color(red: 230/255, green: 230/255, blue: 230/255))
+                    .cornerRadius(7)
+                    .frame(width: 300.0, height: 100.0)
+                Spacer()
+                
+                Button("Log In") {
+                    print("hi")
+                    register()
+                }
+                .alert("Message from SwiftUI", isPresented: $loginSuccess) {
+                            Button("Success") { }
+                        }
+                .padding(10)
+                .frame(width:350)
+                .background(Color(red: 235/255, green: 136/255, blue: 66/255))
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+                .padding(20)
+                
+            }
         }
     }
+        
     func register() {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if error != nil {
--- EventiLatte/Views/notiView.swift
+++ EventiLatte/Views/notiView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct notiView: View {
+    @State var selectedDate = Date()
+        let notify = NotificationHandler()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+                   Spacer()
+                   Button("Send notification in 5 seconds") {
+                       notify.sendNotification(
+                           date: Date(),
+                           type: "time",
+                           timeInterval: 5,
+                           title: "5 second notification",
+                           body: "You can write more in here!")
+                   }
+                   DatePicker("Pick a date:", selection: $selectedDate, in: Date()...)
+                   Button("Schedule notification") {
+                       notify.sendNotification(
+                           date:selectedDate,
+                           type: "date",
+                           title: "Date based notification",
+                           body: "This notification is a reminder that you added a date. Tap on the notification to see more.")
+                   }.tint(.orange)
+                   Spacer()
+                   Text("Not working?")
+                       .foregroundColor(.gray)
+                       .italic()
+                   Button("Request permissions") {
+                       notify.askPermission()
+                   }
+               }
+               .padding()
+           }
     }
-}
+
 
 struct notiView_Previews: PreviewProvider {
     static var previews: some View {
