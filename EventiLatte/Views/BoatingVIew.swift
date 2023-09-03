74620f5b8c321a489956e37767aa45ce117430ac
Kevin Tran <thekevintran01@gmaill.com>
2023-09-03 02:52:56 -0400

    added list to BoatingView

--- EventiLatte/Views/BoatingVIew.swift
+++ EventiLatte/Views/BoatingVIew.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct BoatingVIew: View {
+    @State var DateString = Date.now.formatted(.dateTime.month().day().year())
+    let notify = NotificationHandler()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("colorBackground")
+                .ignoresSafeArea()
+            VStack(alignment: .leading){
+                Text("\(DateString)")
+                    .font(.custom("ArchivoBlack-Regular", size: 24))
+                    .padding(.leading, 10)
+                Text("Today")
+                    .font(.custom("ArchivoBlack-Regular", size: 32))
+                    .padding(.leading, 10)
+                List{
+                    HStack(){
+                        Text("Events")
+                            .font(.custom("ArchivoBlack-Regular", size: 46))
+                        
+                        
+                    }.listRowSeparator(.hidden)
+                        .listRowBackground(Color("colorBackground"))
+                }.background(Color("colorBackground"))
+                    .scrollContentBackground(.hidden)
+            }
+        }
     }
 }
-
 struct BoatingVIew_Previews: PreviewProvider {
     static var previews: some View {
         BoatingVIew()
